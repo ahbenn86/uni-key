9a27a57f72d075aa405950aecc297f0aaa79ca4f
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2023-03-26 17:43:53 -0400

    created modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Container, Box } from "@chakra-ui/react";
 import Header from "../components/Header";
 import TextInput from "../components/TextInput";
 import Footer from "../components/Footer";
+import KeywordsModal from "../components/KeywordsModal";
 
 const App = () => {
   const [keywords, setKeywords] = useState('');
@@ -39,6 +40,10 @@ const App = () => {
     setLoading(false);
   };
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <Box bg={"whitesmoke"} color="black" height="100vh" paddingTop={130}>
       <Container maxW='3xl' centerContent>
@@ -46,6 +51,12 @@ const App = () => {
         <TextInput extractKeywords={extractKeywords}/>
         <Footer/>
       </Container>
+      <KeywordsModal
+      keywords={keywords}
+      loading={loading}
+      isOpen={isOpen}
+      closeModal={closeModal}
+      />
     </Box>
   );
 };
